d4f2a8672b51bfdd09b525f9bf596df875117462
Ros4eva <prosper.sopuruchi@gmail.com>
2019-10-24 05:10:37 +0100

    updates

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  scrumUserLoginData = {}
+  scrumUserLoginData = {};
 
   constructor(private _scrumdataservice: ScrumdataService, private _router: Router) { }
 
